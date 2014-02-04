29be902b7025d409584ee4db2df9b38f18d69901
Jon Brennecke <brennecke.jonathan@wsu.edu>
2014-02-04 12:54:30 -0800

    working output

--- Timed_Intervals/export/WhiskerOpto.m
+++ Timed_Intervals/export/WhiskerOpto.m
@@ -1,81 +1,113 @@
 %=========================================================================
-% Main analysis script for the 'Whisker-Opto Sleep Deprivation' experiment
+% Main analysis script for the 'Thy1xCD1 Whisker/Opto Sleep Deprivation' experiment
 %
 % created by @jonbrennecke / http://github.com/jonbrennecke
 %=========================================================================
 
-addpath ./matlab-utils;
-utils = getUtils;
-[Excel, Workbooks, Sheets] = utils.xl.new();
-sheets = utils.xl.addSheets(Excel,{ '5min Intervals' });
-utils.xl.set(sheets{1},[1,1],{ 'Filename', 'Group', 'Dat' });
+clear
 
-% open the file modal and get some '.txt' files
+% look for 'matlab-utils' in the matlab folder
+addpath ../../../Matlab/etc/matlab-utils/;
+
+% create a new XL object
+xl = XL;
+sheets = xl.addSheets({ '5min Intervals' });
+
+% create a header row with 'Data 1-72'
+% TODO exchange 'data' for time... mod(10+(0:5:360)/60,12) or something
+data = [ char(ones(72,1) * 'Data ' ) num2str( [1:72]' ) ];
+xl.setCells( sheets{1}, [1,1], [ { 'Filename', 'Group' } mat2cell(data,ones(1,72))' ] );
+
+% open the file dialog and get some '.txt' files
 [files,path] = uigetfile({'*txt','Text Files (*.txt)';'*.*','All Files'; },'Select Data File(s)','MultiSelect','On');
-if isstr(files), files = {files}; end
-
-% [ char(ones(72,1)*'Data '), [1:72]' ]
+if ischar(files), files = {files}; end
 
 for i=1:length(files)
 	
 	% extract meaningful data from the filename
-	regexp(files{i},'(.*?)\s+(#\d+)\s+(\d+-\d+-\d+)\s+(\w+)\s+(\w+)\.(\w+)','tokens');
+	nameparts = regexp(files{i},'(.*?)\s+(#\d+)\s+(\d+-\d+-\d+)\s+(\w+)\s+(\w+)\.(\w+)','tokens');
 	nameparts = nameparts{1,1};
 
 	% and write to Excel
-	utils.xl.set(sheets{1},[1,i+1],{ files{i}, nameparts{4} });
+    xl.setCells(sheets{1},[1,i+1],{ files{i}, nameparts{4} });
 
-	lines = utils.os.open([ path files{i} ]);
+    % open the file as a cell array of lines (akin to Python's 'for line in
+    % file' )
+	lines = OS.open([ path files{i} ]);
+    
+    % we want to find the time in the file closest to 10:00 AM, and start there
+    % extract the timestamp from the first line
+    firstLine = Utils.split(lines{3}','\t');
+    startTime = DateTime( firstLine{1} );
+
+    % create a new DateTime object with the same date as 'startTime', but set time to 10:00 AM
+    tenAM = startTime.clone();
+    tenAM.hour = 10; tenAM.minute = 0; tenAM.second = 0;
+
+    % if startTime is equal to or after 10AM, use startTime,
+    % otherwise, find 10AM in the file and start there
+    % FYI: DateTime.cmp() currently returns the number of seconds between the
+    % DateTime objects
+    if( tenAM.cmp(startTime) > 0 )
+        
+        % the file starts *before* 10AM, so we need to find 10:00 AM in the
+        % file
+        for j=3:length(lines)
+            line = Utils.split(lines{j}','\t'); 
+            if( tenAM.cmp( DateTime( line{1} ) ) == 0 )
+                lineStart = j;
+                break
+            end
+        end
+    else
+        
+        % the file starts after or at 10:00 AM, so just start on the first
+        % line of the file
+        lineStart = 1;
+    end
 
 	% create several data arrays (for each sensor) from the file
-	for j=1:length(lines)
-		lines{j} = utils.std.split(lines{j}','\t'); 
+	for j=lineStart:length(lines)
+		lines{j} = Utils.split(lines{j}','\t'); 
 		if j>2
-			bio(j) = str2num( lines{j,1}{3} );
-			eeg1(j) = mean( str2num( [lines{j,1}{4:43}] ) );
-			eeg2(j) = mean( str2num( [lines{j,1}{44:end-1}] ) );
-			emg(j) = str2num( lines{j,1}{end} );
+			bio(j - lineStart + 1) = str2num( lines{j,1}{3} );
+			eeg1(j - lineStart + 1) = mean( str2num( [lines{j,1}{4:43}] ) );
+			eeg2(j - lineStart + 1) = mean( str2num( [lines{j,1}{44:end-1}] ) );
+			emg(j - lineStart + 1) = str2num( lines{j,1}{end} );
 		end
 	end
 
-	% the recording goes from 5am to 5pm, but we want to start sampling at 10am and end at 4pm
-	% 10am is 5 hours into the recording, and
-	% 5 hrs @ 6 samples / min => 1800 samples into the recording
-	% 4pm is 1hr from the end of the recording, and 
-	% 1 hr @ 6 samples / min => 360 samples from the end
-
-	% toss out the first two elements (empty)
-	% so start at '1800 + 3 'and end at 'end - 360'
-	bio = bio(1803:end-360);
-	emg = emg(1803:end-360);
-	eeg1 = eeg1(1803:end-360);
-	eeg2 = eeg2(1803:end-360);
+    % the data in the sensor arrays now starts at 10:00 AM
+    % to select the 6hr block we determine the following:
+    % 6 hours * 60 min / hr * 6 samples / min = 2160 samples
+	bio = bio(1:2160);
+	emg = emg(1:2160);
+	eeg1 = eeg1(1:2160);
+	eeg2 = eeg2(1:2160);
 
 	% divide the data into chunks of 30 samples
 	% since 5 min * 6 samples/min = 30 samples
 	% then average each 5 min window to one sample
-	bio = mean( utils.std.slice(bio,30), 2);
-	emg = mean( utils.std.slice(emg,30), 2);
-	eeg1 = mean( utils.std.slice(eeg1,30), 2);
-	eeg2 = mean( utils.std.slice(eeg2,30), 2);
-
-	ival = 6 / 72;
+	bio = mean( Utils.slice(bio,30), 2);
+	emg = mean( Utils.slice(emg,30), 2);
+	eeg1 = mean( Utils.slice(eeg1,30), 2);
+	eeg2 = mean( Utils.slice(eeg2,30), 2);
 
 	% plotting...
-	% figure; hold on;
-	% set(gca,'Color','black');
-	% title('EEGs and EMG')
-	% plot([ival:ival:6],emg,'g')
-	% plot([ival:ival:6],eeg1,'b')
-	% plot([ival:ival:6],eeg2,'y')
-
-	% figure; hold on;
-	% title('Bio Sensor')
-	% set(gca,'Color','black');
-	% plot([ival:ival:6],bio,'r')
-
+%   ival = 6 / 72;
+% 	figure; hold on;
+% 	set(gca,'Color','black');
+% 	title('EEGs and EMG')
+% 	plot([ival:ival:6],emg,'g')
+% 	plot([ival:ival:6],eeg1,'b')
+% 	plot([ival:ival:6],eeg2,'y')
+% 
+% 	figure; hold on;
+% 	title('Bio Sensor')
+% 	set(gca,'Color','black');
+% 	plot([ival:ival:6],bio,'r')
 
 	% finally output the biosensor data to Excel
-	utils.xl.set(sheets{1},[3,i+1],{ bio' });
+    xl.setCells(sheets{1},[3,i+1],bio');
 
 end
