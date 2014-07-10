3621e5d7e594ed7f2b71173cd3fbdefc490d98ff
mrempe <mjrempe@gmail.com>
2014-07-10 16:54:25 -0700

    sleepscorePCA uses eigs

--- Timed_Intervals/internal/sleepscorePCA.m
+++ Timed_Intervals/internal/sleepscorePCA.m
@@ -17,7 +17,6 @@ function sleepscorePCA(inputfile,signal)
 	% check into using gname.m 
 
 
-	
 
 
 
@@ -57,15 +56,69 @@ end
 
 
 % Finally, do the PCA (using svd, the default for pca.m)
-[wcoeff,score,latent,tsquared,explained] = pca(Feature);
-
 
 
 % Now plot the points along the three eigenvectors with the 3 
 % largest eigenvalues of the covariance matrix
+%figure
+% plot3(score(:,1),score(:,2),score(:,3),'.')
+% xlabel('PC1')
+% ylabel('PC2')
+
+% figure
+% plot3(score2(:,1),score2(:,2),score2(:,3),'.')
+% xlabel('PC1')
+% ylabel('PC2')
+% zlabel('PC3')
+
+
+% Try it again using eigs
+ tic
+ k=3;   % number of principal compenents to keep
+ C=Feature*Feature';    % covariance matrix
+ size(C)
+ [W,L]=eigs(C,k);    %compute the k largest eigenvalues/eigenvectors
+ Wt=W';
+ Y=Wt*Feature;
+FeaturesPCA = W*Y;  %FeaturesPCA contains the same number of observations as Features, but only k columns
+% 	                       % The columns of FeaturesPCA are the first k principal components.
+ figure
+ plot3(FeaturesPCA(:,1),FeaturesPCA(:,2),FeaturesPCA(:,3),'.')
+ xlabel('PC1')
+ ylabel('PC2')
+ zlabel('PC3')
+toc
+
+%Try it a third way, normalizing the data first
+%and using svd.  (This is too slow so I abandoned it)
+% k=3;   % number of principal compenents to keep
+% [m,n]=size(Feature);
+% mnFeature = mean(Feature,2);   % row mean
+% X = Feature - repmat(mnFeature,1,n);  % subtract row mean
+% Z = 1/sqrt(n-1)*X';   %
+% C=Z'*Z;    % covariance matrix
+% [U,S,V] = svd(C);
+% VV=V(:,1:k);
+% Y=VV'*X;
+% XX=VV*Y;
+% XX=XX+repmat(mn,1,n);  %add row means back on
+
+
+tic
+% Try it using eigs, but also normalizing first
+k=3;   % number of principal compenents to keep
+[m,n]=size(Feature);
+mnFeature = mean(Feature,2);   % row mean
+X = Feature - repmat(mnFeature,1,n);  % subtract row mean
+Z = 1/sqrt(n-1)*X';   %
+C2=Z'*Z;    % covariance matrix
+[W2,L2]=eigs(C2,k);    %compute the k largest eigenvalues/eigenvectors
+ Wt2=W2';
+ Y=Wt2*X;
+FeaturesPCAnormed = W2*Y;
 figure
-plot3(score(:,1),score(:,2),score(:,3),'.')
+plot3(FeaturesPCAnormed(:,1),FeaturesPCAnormed(:,2),FeaturesPCAnormed(:,3),'.')
 xlabel('PC1')
 ylabel('PC2')
 zlabel('PC3')
-
+toc
