dd883a763ce3b90a7860f73bca037d6c856b513e
mrempe <mjrempe@gmail.com>
2014-07-11 14:13:38 -0700

    sleepscorePCA now writes to a file and plots

--- Timed_Intervals/internal/sleepscorePCA.m
+++ Timed_Intervals/internal/sleepscorePCA.m
@@ -1,4 +1,4 @@
-function sleepscorePCA(inputfile,signal)
+function [Feature,PCAvectors]=sleepscorePCA(inputfile,signal,outputfilename)
 
 	% This function uses Principal Component Analysis (PCA) to automatically
 	% score a sleep data set (following Gilmour et al 2010)
@@ -9,7 +9,9 @@ function sleepscorePCA(inputfile,signal)
 	%				For now I'm assuming that this .txt file has the following columns: 
 	%				TimeStamp	SleepState	Lactate	EEG1	EEG1 ... EEG2	EEG2 ... EMG
 	%
-	%signal			either EEG1 or EEG2 specifying which signal to use
+	% signal			either EEG1 or EEG2 specifying which signal to use
+	% outputfilename 	the name of the file to which the Feature Matrix and PCA vectors are written  
+
 
 	% TODO:
 	% Make a snazzy user interface so you can just click on data points plotted along the principal 
@@ -39,31 +41,33 @@ if strcmp(signal,'EEG1')
 	Feature(:,2) = mean(data(:,7:10),2);	%theta
 	Feature(:,3) = mean(data(:,12:21),2);	%low beta
 	Feature(:,4) = mean(data(:,32:41),2);	%high beta
-	Feature(:,5) = data(:,82);
+	Feature(:,5) = data(:,82);				%EMG
 	Feature(:,6) = Feature(:,2)./Feature(:,1);
 	Feature(:,7) = mean(data(:,17:31),2)./Feature(:,1);
 end 
 
-if strcmp(signal,'EEG2')  %CHANGE THESE TO REALLY USE EEG2
+if strcmp(signal,'EEG2')  
 	Feature(:,1) = mean(data(:,43:45),2);	%delta
 	Feature(:,2) = mean(data(:,47:50),2);	%theta
 	Feature(:,3) = mean(data(:,52:61),2);	%low beta
 	Feature(:,4) = mean(data(:,72:81),2);	%high beta
-	Feature(:,5) = data(:,82);
+	Feature(:,5) = data(:,82);				%EMG
 	Feature(:,6) = Feature(:,2)./Feature(:,1);
 	Feature(:,7) = mean(data(:,57:71),2)./Feature(:,1);
 end
 
 
 % Finally, do the PCA (using svd, the default for pca.m)
-
+[Coeff,score,latent,tsquared,explained]=pca(Feature);
+explained
 
 % Now plot the points along the three eigenvectors with the 3 
 % largest eigenvalues of the covariance matrix
-%figure
-% plot3(score(:,1),score(:,2),score(:,3),'.')
-% xlabel('PC1')
-% ylabel('PC2')
+figure
+plot3(score(:,1),score(:,2),score(:,3),'.')
+xlabel('PC1')
+ylabel('PC2')
+
 
 % figure
 % plot3(score2(:,1),score2(:,2),score2(:,3),'.')
@@ -76,7 +80,7 @@ end
  tic
  k=3;   % number of principal compenents to keep
  C=Feature*Feature';    % covariance matrix
- size(C)
+ disp(['The Covariance matrix is ', num2str(size(C,1)), ' by ', num2str(size(C,2))])
  [W,L]=eigs(C,k);    %compute the k largest eigenvalues/eigenvectors
  Wt=W';
  Y=Wt*Feature;
@@ -122,3 +126,121 @@ xlabel('PC1')
 ylabel('PC2')
 zlabel('PC3')
 toc
+
+PCAvectors = FeaturesPCAnormed;
+
+
+% Make a plot of all the vectors together in subplots
+figure
+t=linspace(0,(1/360)*size(Feature,1),size(Feature,1));
+subplot(10,1,1)
+plot(t,PCAvectors(:,1),'r')
+ylabel('PCA 1')
+set(gca,'YTickLabel',[])
+set(gca,'YTick',[])
+set(gca,'XTickLabel',[])
+set(gca,'XTick',[])
+box off
+set(get(gca,'YLabel'),'Rotation',0)
+
+subplot(10,1,2)
+plot(t,PCAvectors(:,2),'g')
+ylabel('PCA 2')
+set(gca,'YTickLabel',[])
+set(gca,'YTick',[])
+set(gca,'XTickLabel',[])
+set(gca,'XTick',[])
+box off
+set(get(gca,'YLabel'),'Rotation',0)
+
+subplot(10,1,3)
+plot(t,PCAvectors(:,3),'b')
+ylabel('PCA 3')
+set(gca,'YTickLabel',[])
+set(gca,'YTick',[])
+set(gca,'XTickLabel',[])
+set(gca,'XTick',[])
+box off
+set(get(gca,'YLabel'),'Rotation',0)
+
+subplot(10,1,4)
+plot(t,Feature(:,7),'k')
+ylabel('Beta/Delta')
+set(gca,'YTickLabel',[])
+set(gca,'YTick',[])
+set(gca,'XTickLabel',[])
+set(gca,'XTick',[])
+box off
+set(get(gca,'YLabel'),'Rotation',0)
+
+subplot(10,1,5)
+plot(t,Feature(:,6),'k')
+ylabel('Theta/Delta')
+set(gca,'YTickLabel',[])
+set(gca,'YTick',[])
+set(gca,'XTickLabel',[])
+set(gca,'XTick',[])
+box off
+set(get(gca,'YLabel'),'Rotation',0)
+
+subplot(10,1,6)
+plot(t,Feature(:,5),'k')
+ylabel('EMG')
+set(gca,'YTickLabel',[])
+set(gca,'YTick',[])
+set(gca,'XTickLabel',[])
+set(gca,'XTick',[])
+box off
+set(get(gca,'YLabel'),'Rotation',0)
+
+subplot(10,1,7)
+plot(t,Feature(:,4),'k')
+ylabel('High Beta')
+set(gca,'YTickLabel',[])
+set(gca,'YTick',[])
+set(gca,'XTickLabel',[])
+set(gca,'XTick',[])
+box off
+set(get(gca,'YLabel'),'Rotation',0)
+
+subplot(10,1,8)
+plot(t,Feature(:,3),'k')
+ylabel('Low Beta')
+set(gca,'YTickLabel',[])
+set(gca,'YTick',[])
+set(gca,'XTickLabel',[])
+set(gca,'XTick',[])
+box off
+set(get(gca,'YLabel'),'Rotation',0)
+
+subplot(10,1,9)
+plot(t,Feature(:,2),'k')
+ylabel('Theta')
+set(gca,'YTickLabel',[])
+set(gca,'YTick',[])
+set(gca,'XTickLabel',[])
+set(gca,'XTick',[])
+box off
+set(get(gca,'YLabel'),'Rotation',0)
+
+subplot(10,1,10)
+plot(t,Feature(:,1),'k')
+ylabel('Delta')
+set(gca,'YTickLabel',[])
+set(gca,'YTick',[])
+box off
+set(get(gca,'YLabel'),'Rotation',0)
+
+% remove white space in-between plots by expanding each subplot (I stole this)
+nF=1.3;  % factor to scale by
+hAx = findobj(gcf, 'type', 'axes');
+for h = 1:length(hAx)
+    vCurrPos = get(hAx(h), 'position'); % current position
+    set(hAx(h), 'position', (vCurrPos.*[1 1 nF nF])-[vCurrPos(3)*(nF-1)/2 vCurrPos(4)*(nF-1)/2 0 0]);
+end
+
+
+%Finally, write out the tab-delimited PCA data as well as the Feature matrix
+% Rows are data points, Cols are Organized as follows: Delta	Theta	LowBeta	HighBeta	EMG	Theta/Delta	Beta/Delta	PCA1	PCA2 etc.
+dlmwrite(outputfilename,[Feature PCAvectors],'delimiter','\t')
+
